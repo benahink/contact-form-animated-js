f1b5e8e57d885a7e3def47008d9a3a96b2c496f2
benahink <benahink@gmail.com>
2024-01-03 23:55:53 -0700

    added svg w/css

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const inputs = document.querySelectorAll(".contact-input");
 inputs.forEach(input => {
   input.addEventListener("focus", () => {
   	input.parentNode.classList.add("focus");
+
+  	// will change font-size for the input label 
+  	input.parentNode.classList.add("not-empty");
   });
   input.addEventListener("blur", () => {
+  	// will remove font-size for the input label 
+  	if(input.value == "") {
+  	  input.parentNode.classList.remove("not-empty");
+  	}
   	input.parentNode.classList.remove("focus");
   });
 });
